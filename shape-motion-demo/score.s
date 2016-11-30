71cf8e0bf9d76475267978f8aad3af7f870a3787
raul4h <rjhinostrozagarcia@miners.utep.edu>
2016-11-29 22:27:39 -0700

    Update score.s

--- shape-motion-demo/score.s
+++ shape-motion-demo/score.s
@@ -5,8 +5,8 @@
 	.global increaseScoreBlue
 
 increaseScoreRed:
-	add.b #1,5(r12)
+	add.b #1,5(r12)		;scoreRed[5]++
 	ret
 increaseScoreBlue:
-	add.b #1,6(r12)
+	add.b #1,6(r12)		;scoreBlue[6]++
 	ret
